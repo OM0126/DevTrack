b57fcc2982d21441b7725b71c7a9e5d15dbb38ab
om yengantiwar <omgithub01@gmail.com>
2026-08-06 23:03:38 +0530

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Today from './pages/Today';
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<AppLayout />}>
+      <Route path="/" element={<AppLayout.. />}>
         <Route index element={<Today />} />
       </Route>
     </Routes>
